3ac3c781eb407db865e2234c4c2281e7f02d78f9
smith-chris <kowalski.k@outlook.com>
2018-08-17 17:56:26 +0100

    Map is generated based on data in the store. Next: collisions.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 import { Container } from 'react-pixi-fiber'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { gameActions, Direction } from 'store/game'
+import { gameActions, Direction, GameState } from 'store/game'
 import { Point } from 'utils/point'
-import { getMap } from './getMap'
 import { palletTown } from 'assets/maps'
 import { getNextPosition } from 'store/gameTransforms/move'
 import { Transition } from './Transition'
 import { TILE_SIZE } from 'assets/const'
 import { SCREEN_SIZE } from 'app/app'
 import { createPointStepper } from 'utils/transition'
+import { Sprite } from 'utils/fiber'
+import { TILESETS } from 'assets/tilesets'
 
 // WIP: The map name should be stored in redux
 export const CURRENT_MAP = palletTown
@@ -19,8 +20,6 @@ export const canMove = (position: Point, direction: Direction) => {
   const { x, y } = getNextPosition(direction, position)
   return Boolean(CURRENT_MAP.collisions[`${x}_${y}`])
 }
-
-const MAP_COMPONENT = getMap(CURRENT_MAP)
 
 const mapStateToProps = (state: StoreState) => state
 type StateProps = ReturnType<typeof mapStateToProps>
@@ -42,27 +41,75 @@ const MAP_CENTER = {
 const getMapPosition = (player: Point) =>
   new Point(MAP_CENTER.x - player.x * 16, MAP_CENTER.y - player.y * 16)
 
-class MapComponent extends Component<Props> {
-  shouldComponentUpdate({
-    game: {
-      player: { position: newPosition, destination: newDestination },
-    },
-  }: Props) {
+const makeMap = (game: GameState) => {
+  if (!game.currentMap) {
+    return null
+  }
+  const result: JSX.Element[] = []
+  game.currentMap.textureIds.forEach((row, x) => {
+    row.forEach((textureId, y) => {
+      result.push(
+        <Sprite
+          key={`${x}x${y}`}
+          position={new Point(x * 8, y * 8)}
+          texture={TILESETS.OVERWORLD.cutTexture(
+            (textureId % 16) * 8,
+            Math.floor(textureId / 16) * 8,
+            8,
+            8,
+          )}
+        />,
+      )
+    })
+  })
+  return result
+}
+
+type State = { map: ReturnType<typeof makeMap> }
+
+class MapComponent extends Component<Props, State> {
+  state = {
+    map: null,
+  }
+  shouldComponentUpdate(
+    {
+      game: {
+        player: { position: newPosition, destination: newDestination },
+      },
+    }: Props,
+    newState: State,
+  ) {
     const {
       game: {
         player: { position, destination },
       },
     } = this.props
-    return position !== newPosition || destination !== newDestination
+    return (
+      position !== newPosition ||
+      destination !== newDestination ||
+      this.state !== newState
+    )
+  }
+  componentWillMount() {
+    this.setState({
+      map: makeMap(this.props.game),
+    })
+  }
+  componentWillReceiveProps({ game: newGame }: Props) {
+    const { game } = this.props
+
+    if (game.currentMap !== newGame.currentMap && newGame.currentMap) {
+      this.setState({ map: makeMap(newGame) })
+    }
   }
   render() {
     const {
-      game: { player, currentMap },
+      game: { player },
       moveEnd,
     } = this.props
-
-    if (!(currentMap && currentMap.textureIds)) {
-      return
+    const { map } = this.state
+    if (!map) {
+      return null
     }
 
     const stepper = player.destination
@@ -78,7 +125,7 @@ class MapComponent extends Component<Props> {
         stepper={stepper}
         useTicks
         onFinish={moveEnd}
-        render={position => <Container position={position}>{MAP_COMPONENT}</Container>}
+        render={position => <Container position={position}>{map}</Container>}
       />
     )
   }
