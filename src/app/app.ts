f8240d968d986ecfaf4ed01f89f44909a1118f7c
smith-chris <kowalski.k@outlook.com>
2018-10-21 12:58:28 +0100

    Safari fix.

--- src/app/app.ts
+++ src/app/app.ts
@@ -24,19 +24,10 @@ if (DEBUG_MAP_BOUNDS && appElement) {
   appElement.classList.add(styles.debugBounds)
 }
 
-export const viewport = !DEBUG_MAP_BOUNDS
-  ? App.renderer.screen
-  : {
-      get width() {
-        return App.renderer.screen.width - DEBUG_OFFSET * 2
-      },
-      get height() {
-        return App.renderer.screen.height - DEBUG_OFFSET * 2
-      },
-    }
+export const viewport = { width: SCREEN_SIZE_FACTOR, height: SCREEN_SIZE_FACTOR }
 
-const getClosestMultiplication = (base: number, max: number) =>
-  ((max - (max % base)) / base + 1) * base
+const getClosestMultiplier = (base: number, max: number) =>
+  (max - (max % base)) / base + 1
 
 if (!appElement) {
   console.warn('App element not found..')
@@ -49,23 +40,27 @@ if (!appElement) {
   const resize = () => {
     const width = window.innerWidth
     const height = window.innerHeight
-    let rendererWidth = SCREEN_SIZE_FACTOR
-    let rendererHeight = SCREEN_SIZE_FACTOR
+    viewport.width = SCREEN_SIZE_FACTOR
+    viewport.height = SCREEN_SIZE_FACTOR
     if (width > height) {
-      rendererWidth = SCREEN_SIZE_FACTOR * (width / height)
+      viewport.width = Math.floor(SCREEN_SIZE_FACTOR * (width / height))
     } else if (height > width) {
-      rendererHeight = SCREEN_SIZE_FACTOR * (height / width)
+      viewport.height = Math.floor(SCREEN_SIZE_FACTOR * (height / width))
     }
     canvasElement.style.width = `${width}px`
     canvasElement.style.height = `${height}px`
 
-    const multiplier = !isSafari
-      ? 1
-      : getClosestMultiplication(SCREEN_SIZE_FACTOR, Math.min(width, height))
-
-    App.renderer.resize(rendererWidth * multiplier, rendererHeight * multiplier)
     if (isSafari) {
+      // Safari doesnt support `image-rendering: pixelated` :(
+      const multiplier = getClosestMultiplier(viewport.width, width)
       App.stage.scale.set(multiplier)
+      App.renderer.resize(viewport.width * multiplier, viewport.height * multiplier)
+    } else {
+      App.renderer.resize(viewport.width, viewport.height)
+    }
+    if (DEBUG_MAP_BOUNDS) {
+      viewport.width -= DEBUG_OFFSET * 2
+      viewport.height -= DEBUG_OFFSET * 2
     }
   }
   resize()
